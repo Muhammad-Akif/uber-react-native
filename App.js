4723581473d95c2500de94a9b194a91a45682c4a
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-22 01:34:18 +0500

    added provider app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uber Clone!</Text>
-    </View>
+    <Provider store=''>
+      <View style={styles.container}>
+        <Text>Uber Clone!</Text>
+      </View>
+    </Provider>
   );
 }
 
