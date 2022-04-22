a184c9e370a1284e26652455ec4e398eb149dce7
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-22 14:55:19 +0500

    setting up with tailwind in react native

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, SafeAreaView, Platform, StatusBar } from 'react-native'
+import tw from 'twrnc';
+
 
 const Home = () => {
     return (
-        <SafeAreaView style={styles.androidSafeArea}>
-            <Text style={styles.text}>Home</Text>
+        <SafeAreaView style={[tw`bg-white h-full`,styles.androidSafeArea]}>
+            <Text style={[tw`text-lg text-green-800 `]}>Home</Text>
         </SafeAreaView>
     )
 }
