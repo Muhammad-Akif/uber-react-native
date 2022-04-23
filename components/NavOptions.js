60e93e0e24a11f501992122f12bdbce84881dede
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-23 15:16:39 +0500

    added data and UI bsics in component

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
 const NavOptions = () => {
-  return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
-  )
+    const data = [
+        {
+            id: "123",
+            title: "Get a ride",
+            image: "https://links.papareact.com/3pn",
+            screen: "MapScreen"
+        },
+        {
+            id: "456",
+            title: "Order food",
+            image: "https://links.papareact.com/28w",
+            screen: "EatsScreen"
+        }
+    ]
+    return (
+        <FlatList
+            data={data}
+            horizontal
+            keyExtractor={(item) => item.id}
+            renderItem={(item) => (
+                <TouchableOpacity>
+                    <View>
+                        <Image 
+                        style={{ width:120, height:120, resizeMode:'contain' }}
+                        source={{uri: item.image}}/>
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    )
 }
 
 export default NavOptions;
