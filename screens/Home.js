47b9da1adafb61f8431249f1ec65fcdbf388bb72
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-23 15:06:44 +0500

    added NavOptions.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { StyleSheet, View, SafeAreaView, Platform, StatusBar, Image } from 'react-native'
 import tw from 'twrnc';
-
+import NavOptions from '../components/NavOptions';
 
 const Home = () => {
     return (
         <SafeAreaView style={[tw`bg-white h-full`, styles.androidSafeArea]}>
             <View style={tw`p-5`}>
                 <Image
-                style={{
-                    width: 100,
-                    height: 100,
-                    resizeMode: "contain"
-                }}
-                source={{
-                    uri: "https://links.papareact.com/gzs"
-                }}
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain"
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs"
+                    }}
                 />
             </View>
+            <NavOptions />
         </SafeAreaView>
     )
 }
