6d3750d6eaf9284ec494d9362f8aa2d03b9c8640
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-24 11:37:17 +0500

    added autocomplete details

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, View, SafeAreaView, Platform, StatusBar, Image } from 'react-native'
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
     return (
@@ -16,6 +18,15 @@ const HomeScreen = () => {
                     source={{
                         uri: "https://links.papareact.com/gzs"
                     }}
+                />
+                <GooglePlacesAutocomplete
+                placeholder="Where From?"
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: "en"
+                }}
+                nearbyPlacesAPI="GooglePlacesSearch"
+                debounce={400}
                 />
             <NavOptions />
             </View>
